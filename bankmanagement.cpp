94ba92d6907b96999d854c4d6137a0e59271de1d
Nikhil Mukati <nikhilmukati123@gmail.com>
2021-10-09 20:06:27 +0530

    Update bankmanagement.cpp

--- bankmanagement.cpp
+++ bankmanagement.cpp
@@ -24,21 +24,7 @@ public:
 	char rettype() const;
 };
 
-void account::create_account()
-{
-	system("CLS");
-	cout<<"\n\t\t\tEnter the Account No. : ";
-	cin>>acno;
-	cout<<"\n\n\t\t\tEnter the Name of the Account holder : ";
-	cin.ignore();
-	cin.getline(name,50);
-	cout<<"\n\t\t\tEnter Type of the Account (C/S) : ";
-	cin>>type;
-	type=toupper(type);
-	cout<<"\n\t\t\tEnter The Initial amount : ";
-	cin>>deposit;
-	cout<<"\n\n\t\t\tAccount Created..";
-}
+
 
 void account::show_account() const
 {
